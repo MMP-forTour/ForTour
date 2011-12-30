5e9da583d02665236811c48f2b1e87b03c2db30c
moneycat <moneycat.tw@gmail.com>
2011-12-30 22:04:22 +0800

    Cleanup close button code. And ready for 1.3 release.

--- FinProj/src/tw/edu/ntu/fortour/EditPageInfo.java
+++ FinProj/src/tw/edu/ntu/fortour/EditPageInfo.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
-import android.widget.Button;
 import android.widget.LinearLayout;
 
 
--- FinProj/src/tw/edu/ntu/fortour/ForTourInfo.java
+++ FinProj/src/tw/edu/ntu/fortour/ForTourInfo.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
-import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
